f5ca28f3f8eda2223020f18edb691ad623e0628c
Aamir-Mallick <52884890+Aamir-Mallick@users.noreply.github.com>
2020-11-17 16:32:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SignUpLogIn from "./components/SignUp-LogIn/SignUpLogIn";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpLogIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp-LogIn/SignUpLogIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignUpLogIn() {
+  return <div>SignUp LogIn Page</div>;
+}
+
+export default SignUpLogIn;
